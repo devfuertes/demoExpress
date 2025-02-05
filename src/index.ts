cf2ea1c53d9795da5ea51e75b5eb1bd46fb6e1f3
devfuertes <webnivel3@gmail.com>
2025-02-05 23:40:22 +0100

    Start app

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,56 @@
-import express from "express";
-const app = express();
+import { createServer } from 'node:http';
+import type { ServerResponse } from 'node:http';
+import 'dotenv/config';
+import createDebug from 'debug';
+import { HtmlError } from './error.js';
+import { createHtmlString } from './template.js';
 
-app.get('/', function (req, res) {
-    res.send('Hello World');
-});
+import { app } from './app.js';
 
-//app.listen(3000);
-const port = 3000;
-app.listen(port, () => {
-    console.log('Server is running on port 3000');});
+const debug = createDebug('demo:server');
+debug('Iniciando servidor...');
+const PORT = process.env.PORT || 3000;
+
+const listenManager = () => {
+    const addr = server.address();
+    if (addr === null) return;
+    let bind: string;
+    if (typeof addr === 'string') {
+        bind = 'pipe ' + addr;
+    } else {
+        bind =
+            addr.address === '::'
+                ? `http://localhost:${addr?.port}`
+                : `${addr.address}:${addr?.port}`;
+    }
+    console.log(`Server listening on ${bind}`);
+    debug(`Servidor escuchando en ${bind}`);
+};
+
+const errorManager = (error: Error | HtmlError, response: ServerResponse) => {
+    if (!('status' in error)) {
+        error = {
+            ...error,
+            statusCode: 500,
+            status: 'Internal Server Error',
+        };
+    }
+
+    const publicMessage = `Error: ${error.statusCode} ${error.status}`;
+    debug(publicMessage, error.message);
+
+    const html = createHtmlString(
+        'Error | Node Server',
+        'Error',
+        publicMessage,
+    );
+    response.statusCode = error.statusCode;
+    response.statusMessage = error.status;
+    response.setHeader('Content-Type', 'text/html; charset=utf-8');
+    response.end(html);
+};
+
+const server = createServer(app);
+server.listen(PORT);
+server.on('listening', listenManager);
+server.on('error', errorManager);
